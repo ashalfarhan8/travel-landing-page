abc9bf44f8fcc08fd0302637bafc6917bb7e3cbd
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-01-17 00:42:38 +0700

    edit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,12 +3,14 @@ import { Link } from "gatsby"
 
 import Layout from "../components/Main/layout"
 import SEO from "../components/Main/seo"
+import Hero2 from "../components/Hero/Hero2"
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <Hero2 />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
